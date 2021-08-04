f59f868ab9b63fa4c23ef5b0e98fce906a2225b3
NizomMeko <rakhimovmeko@gmail.com>
2021-08-04 15:18:51 +0500

    react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App";
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>{this.props.meko} - Academy</h1>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App meko="WebBrain"/>
+    <App meko="PDP"/>
+    <App meko="IT"/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals();
